0dd285a950e40e03e74a44675d362053fc37e823
javier <nhat1234559@gmail.com>
2025-01-10 18:46:56 +0700

    update deploy with github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://javier1234559.github.io',
   integrations: [tailwind(), react()]
 });
